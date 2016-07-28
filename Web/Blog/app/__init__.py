52c826864d5fd05e07eae6c67f2381e60a99ce05
WindArrow <wind.dong.dream@gmail.com>
2016-07-28 23:07:37 +0900

    init id, password input box

--- Web/Blog/app/__init__.py
+++ Web/Blog/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 
 app = Flask(__name__)
+app.secret_key = 'secret'
 
 
 from app.routes import index
--- Web/Blog/app/routes/user.py
+++ Web/Blog/app/routes/user.py
@@ -1,8 +1,12 @@
 from app import app
 from app.managers.url_manager import LOGIN_URL
-from flask import render_template
+from flask import render_template, request
+from app.forms.user import LoginForm
 
 
-@app.route(LOGIN_URL)
+@app.route(LOGIN_URL, methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate_on_submit():
+        return render_template('login.html', form=form)
+    return render_template('login.html', form=form)
