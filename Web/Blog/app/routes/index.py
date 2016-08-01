975c6998b22c2c8212e92a2f8e9db602103d6372
WindArrow <wind.dong.dream@gmail.com>
2016-08-01 23:32:47 +0900

    display_notice

--- Web/Blog/app/routes/index.py
+++ Web/Blog/app/routes/index.py
@@ -1,16 +1,17 @@
 from flask import render_template, session
-
-
 from app import app
 from app.managers.url_manager import LOGIN_URL, SIGN_UP_URL, LOGOUT_URL, WRITE_NOTICE_URL
+from app.models.notice import get_notices
 
 
 @app.route('/')
 def root():
     user = session.get('user')
+    print('notices = {}'.format(get_notices()))
     return render_template('index.html',
                            login_url=LOGIN_URL,
                            sign_up_url=SIGN_UP_URL,
                            user=user,
                            logout_url=LOGOUT_URL,
-                           write_notice_url=WRITE_NOTICE_URL)
+                           write_notice_url=WRITE_NOTICE_URL,
+                           notices=get_notices())
