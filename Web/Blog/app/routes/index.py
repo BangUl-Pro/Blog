550398c99f0b2d5e645326582d05b96675b52647
WindArrow <wind.dong.dream@gmail.com>
2016-08-03 14:54:22 +0900

    category display

--- Web/Blog/app/routes/index.py
+++ Web/Blog/app/routes/index.py
@@ -2,12 +2,12 @@ from flask import render_template, session
 from app import app
 from app.managers.url_manager import LOGIN_URL, SIGN_UP_URL, LOGOUT_URL, WRITE_NOTICE_URL, SETTING_URL
 from app.models.notice import get_notices, get_latest_notices
+from app.models.category import get_categories
 
 
 @app.route('/')
 def root():
     user = session.get('user')
-    print('notices = {}'.format(get_latest_notices()))
     return render_template('index.html',
                            login_url=LOGIN_URL,
                            sign_up_url=SIGN_UP_URL,
@@ -16,4 +16,5 @@ def root():
                            setting_url=SETTING_URL,
                            write_notice_url=WRITE_NOTICE_URL,
                            notices=get_notices(),
+                           categories=get_categories(),
                            latest_notices=get_latest_notices())
